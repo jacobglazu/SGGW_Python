914127d8ca0174b9b55c9fb6ae1d3f45c9d1c23d
jacobglazu <jacobglaz@poczta.fm>
2025-11-01 04:48:06 +0100

    Classa savedivisin, dodanie logowania blad zapisu

--- my_app/src/my_app/Context_Managers.py
+++ my_app/src/my_app/Context_Managers.py
@@ -1,4 +1,5 @@
 import time
+import logging
 
 class Logger:
     def __enter__(self):
@@ -28,17 +29,39 @@ class FileWriter:
         print(f"Zamykam zasób")
         if self.file:
             self.file.close()
+        if exc_val is not None:
+            logging.error(f"Błąd zapisu: {exc_val}")
         
+        return False 
             
+class SafeDivision:
+        
+    def __enter__(self):
+        return self
+    
+    def divide(self,a,b):
 
+        return a/b
+    
+    def __exit__(self, ecx_type, exc_val, exc_tb):
+        if ecx_type is ZeroDivisionError:
+            logging.error(f"Nie można dzielić przez zero")
+        
+            return True
+        return False
+    
+    logging.basicConfig(level=logging.ERROR)
 
-
-"""text = FileWriter('text.txt')
-obj = text.__enter__()
-try:
-    print(obj)
-finally:
-    text.__exit__(ecx_type=None, exc_val=None, exc_tb=None)"""
+#text = FileWriter('text.txt')
+#obj = text.__enter__()
+#try:
+ #   print(obj)
+#finally:
+ #   text.__exit__(ecx_type=None, exc_val=None, exc_tb=None)
 
 with FileWriter('text.txt') as resource: 
     resource.write('Tu LastPass, ')
+
+with SafeDivision() as sd:
+    result = sd.divide(10, 2)
+    print(f"Wynik dzielenia: {result}")
