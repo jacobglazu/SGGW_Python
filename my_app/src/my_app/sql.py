c27ed153e65c3830805fded5416f9f996b9dbec2
jacobglazu <jacobglaz@poczta.fm>
2025-11-06 15:03:18 +0100

    Poprawa relacji

--- my_app/src/my_app/sql.py
+++ my_app/src/my_app/sql.py
@@ -101,13 +101,13 @@ class Experiment2(Base):
     finished: Mapped[Boolean] = mapped_column(Boolean, default=False)
 
   
-    datapoints: Mapped[list["DataPoints"]] = relationship(back_populates="experiment", cascade= "all, delete-orphan",)
+    datapoints: Mapped[list["DataPoints"]] = relationship(back_populates="experiment2", cascade= "all, delete-orphan",)
 class DataPoints(Base):
     __tablename__ = 'datapoints'
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     experiment_id: Mapped[int] = mapped_column(Integer, ForeignKey(Experiment2.id), nullable= False)
     real_value: Mapped[float] = mapped_column(Float(50))
-    target_value: Mapped[float] = mapped_column(Float(50))
+    target_value: Mapped[float] = mapped_column(Float(50))  
 
     experiment : Mapped[Experiment2] = relationship(back_populates="datapoints")
 
@@ -122,6 +122,16 @@ Base.metadata.create_all(engine)
 with Session(engine) as session:
     exp = Experiment2(title="Wood", type=3, finished=False)
     dp1 = DataPoints(real_value=1.23, target_value=2.34, experiment=exp)
+    dp2 = DataPoints(real_value= 2.1, target_value= 3.3, experiment =exp)
+    dp3 = DataPoints(real_value= 2.2, target_value= 3.5, Experiment =exp)
+    dp4 = DataPoints(real_value= 2.5, target_value= 3.6, Experiment =exp)
+    dp5 = DataPoints(real_value= 2.4, target_value= 3.7, Experiment =exp)
+    dp6 = DataPoints(real_value= 2.6, target_value= 3.8, Experiment =exp)
+    dp7 = DataPoints(real_value= 2.3, target_value= 3.23, Experiment =exp)
+    dp8 = DataPoints(real_value= 2.8, target_value= 3.44, Experiment =exp)
+    dp9 = DataPoints(real_value= 2.33, target_value= 3.55, Experiment =exp)
+    dp10 = DataPoints(real_value= 2.44, target_value= 3.15, Experiment =exp)
+
 
     session.add(exp)   # dp1 zostanie dodany automatycznie dzięki cascade
     session.commit()
